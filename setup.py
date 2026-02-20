d38f769cdf3aae2514b0d8b715932e543acc9e48
wademw-web <wadem117@gmail.com>
2026-02-20 15:00:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(packages=find_packages(),
         'requests'
       ],
       include_package_data=True,
-      package_data={"": ["*.txt", "*.csv", "*.sas]},
+      package_data={"": ["*.txt", "*.csv", "*.sas"]},
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: Apache Software License",
